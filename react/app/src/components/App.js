19f88c408a6804d0b3df8a119185cf50c8a19c0c
Shota Itabashi <shota961102@gmail.com>
2018-09-23 23:49:23 +0900

    material-uiを導入

--- react/app/src/components/App.js
+++ react/app/src/components/App.js
@@ -5,16 +5,24 @@ import Top from './Top'
 import Routes from './Routes'
 import Restaurants from './Restaurants'
 import Restaurant from './Restaurant'
+import createMuiTheme from '@material-ui/core/styles'
+import {AppBar, Typography} from '@material-ui/core'
 
 const App = () => (
   <BrowserRouter>
     <div>
-     <ul>
+      <ul>
        <li><Link to='/'>Top</Link></li>
        <li><Link to='/routes'>Routes</Link></li>
        <li><Link to='/restaurants'>Restaurants</Link></li>
        <li><Link to='/restaurant'>Restaurant</Link></li>
-     </ul>
+      </ul>
+
+      <AppBar position="static" color="default">
+        <Typography variant="title" color="inherit">
+          RaiMea
+        </Typography>
+     　</AppBar>
 
       <Route exact path='/' component={Top} />
       <Route exact path='/routes' component={Routes} />
